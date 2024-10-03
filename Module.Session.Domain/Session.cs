11f28e35ca6c7c78db17ab65a2fbce13a79249ec
AndreasOEK <oe.thomsen@gmail.com>
2024-10-03 08:29:28 +0200

    Started on Session Domain

---
+++ Module.Session.Domain/Session.cs
@@ -0,0 +1,43 @@
+﻿namespace Module.Session.Domain;
+
+public class Session
+{
+    public DateTime StartTime { get; protected set; }
+    public DateTime EndTime { get; protected set; }
+    public Trainer AssignedTrainer { get; protected set; }
+    public int AvailableSlots { get; protected set; }
+    public SkillLevel DifficultyLevel { get; protected set; }
+    private List<Booking> _bookings { get; set; }
+    public IEnumerable<Booking> Bookings { get { return _bookings; } private set{ _bookings = value.ToList(); } }
+
+    protected Session() { }
+
+    private Session(DateTime startTime, DateTime endTime, Trainer assignedTrainer, int availableSlots, SkillLevel difficultyLevel)
+    {
+        StartTime = startTime;
+        EndTime = endTime;
+        AssignedTrainer = assignedTrainer;
+        AvailableSlots = availableSlots;
+        DifficultyLevel = difficultyLevel;
+    }
+
+    public static Session Create(DateTime startTime, DateTime endTime, Trainer assignedTrainer, int availableSlots, SkillLevel difficultyLevel)
+    {
+        return new Session(startTime, endTime, assignedTrainer, availableSlots, difficultyLevel);
+    }
+
+    public void AddBooking(Booking booking)
+    {
+
+    }
+}
+
+public enum SkillLevel
+{
+    Beginner,
+    Novice,
+    Intermediate,
+    Advanced,
+    Expert,
+    Professional
+}
