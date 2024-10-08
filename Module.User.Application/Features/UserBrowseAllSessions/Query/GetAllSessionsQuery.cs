59fba5e3a79ff65e1b84b7e78c006d2466361fa6
AndreasOEK <oe.thomsen@gmail.com>
2024-10-08 09:34:55 +0200

    Handler handled

--- Module.User.Application/Features/UserBrowseAllSessions/Query/GetAllSessionsQuery.cs
+++ Module.User.Application/Features/UserBrowseAllSessions/Query/GetAllSessionsQuery.cs
@@ -1,5 +1,4 @@
 ﻿using MediatR;
-using Module.User.Application.Abstractions;
 using Module.User.Application.Features.UserBrowseAllSessions.Query.Dto;
 
 namespace Module.User.Application.Features.UserBrowseAllSessions.Query
@@ -7,22 +6,5 @@ namespace Module.User.Application.Features.UserBrowseAllSessions.Query
     public record GetAllSessionsQuery : IRequest<IEnumerable<SessionResponse>>
     {
 
-    }
-
-    public class GetAllSessionsQueryHandler : IRequestHandler<GetAllSessionsQuery, IEnumerable<SessionResponse>>
-    {
-        private readonly ISessionRepository _sessionRepository;
-
-        public GetAllSessionsQueryHandler(ISessionRepository sessionRepository)
-        {
-            _sessionRepository = sessionRepository;
-        }
-
-        async Task<IEnumerable<SessionResponse>> IRequestHandler<GetAllSessionsQuery, IEnumerable<SessionResponse>>.Handle(
-            GetAllSessionsQuery request,
-            CancellationToken cancellationToken)
-        {
-            return await _sessionRepository.GetAllSessions();
-        }
     }
 }
