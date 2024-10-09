4536c9ba7a3f9c7b7ae4ebebadd05d068fec6cc6
AndreasOEK <oe.thomsen@gmail.com>
2024-10-09 13:01:32 +0200

    Add Ignored Files

---
+++ Module.User/Endpoints/CreateSession.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Module.Shared.Abstractions;
+using Module.User.Application.Features.TrainerSession.Command;
+using Module.User.Application.Features.TrainerSession.Command.Dto;
+
+namespace Module.User.Endpoints
+{
+    public class CreateSession : IEndpoint
+    {
+        void IEndpoint.MapEndpoint(WebApplication app)
+        {
+            app.MapPost("/Session", async ([FromBody] CreateSessionRequest createSessionRequest, [FromServices] IMediator mediator) =>
+                await mediator.Send(new CreateSessionCommand(createSessionRequest))
+            ).WithTags("Session");
+        }
+    }
+}
