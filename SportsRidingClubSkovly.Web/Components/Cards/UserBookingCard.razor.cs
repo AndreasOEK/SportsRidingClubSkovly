770b4f0a469994645b49dad6dee2584190c50330
Daniel From Kjærgaard <KongzCringe@gmail.com>
2024-10-15 17:20:36 +0200

    profile page
    Open session of booking | Book a session from profile

--- SportsRidingClubSkovly.Web/Components/Cards/UserBookingCard.razor.cs
+++ SportsRidingClubSkovly.Web/Components/Cards/UserBookingCard.razor.cs
@@ -6,4 +6,10 @@ namespace SportsRidingClubSkovly.Web.Components.Cards;
 public partial class UserBookingCard : ComponentBase
 {
     [Parameter] public UserBookingResponse Booking { get; set; }
+    [Inject] public NavigationManager NavigationManager { get; set; }
+    
+    private void NavigateToSession()
+    {
+        NavigationManager.NavigateTo($"Session/{Booking.Session.Id}"); 
+    }
 }
---
+++ SportsRidingClubSkovly.Web/Components/Pages/BrowseSessions.razor.cs
@@ -0,0 +1,18 @@
+﻿using SportsRidingClubSkovly.Web.DTO.UserSession;
+
+namespace SportsRidingClubSkovly.Web.Components.Pages;
+
+public partial class BrowseSessions
+{
+    private IEnumerable<SessionResponse> sessions = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        sessions = await userSessionProxy.GetSessionsAsync();
+    }
+
+    public int CalculateSlotsLeft(SessionResponse session)
+    {
+        return session.MaxNumberOfParticipants - session.Bookings.Count();
+    }
+}
