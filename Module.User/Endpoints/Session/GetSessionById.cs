1f591528551b87201322878974020138ba623c9c
AndreasOEK <oe.thomsen@gmail.com>
2024-10-15 16:18:02 +0200

    EndPoint GetSessionById

---
+++ Module.User/Endpoints/Session/GetSessionById.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Module.Shared.Abstractions;
+using Module.User.Application.Features.UserSession.Query;
+
+namespace Module.User.Endpoints.Session
+{
+    class GetSessionById : IEndpoint
+    {
+        void IEndpoint.MapEndpoint(WebApplication app)
+        {
+            app.MapGet("/Session/{sessionId}", async (Guid sessionId, IMediator mediator) =>
+            {
+                return await mediator.Send(new GetSessionByIdQuery(sessionId));
+            }).WithTags("Session");
+        }
+    }
+}
