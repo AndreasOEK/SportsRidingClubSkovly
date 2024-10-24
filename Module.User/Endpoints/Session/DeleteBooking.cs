ae452e3a90bc7a9cf2e380b1fdf477a63602e810
AndreasOEK <oe.thomsen@gmail.com>
2024-10-24 09:34:53 +0200

    Added som Ignored Files

---
+++ Module.User/Endpoints/Session/DeleteBooking.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Module.Shared.Abstractions;
+using Module.User.Application.Features.UserSession.Command;
+using Module.User.Application.Features.UserSession.Command.Dto;
+
+namespace Module.User.Endpoints.Session;
+
+public class DeleteBooking : IEndpoint
+{
+    public void MapEndpoint(WebApplication app)
+    {
+        app.MapDelete("/Session/Booking", async ([FromBody]DeleteBookingRequest deleteBookingRequest, [FromServices] IMediator mediator) =>
+            await mediator.Send(new DeleteBookingCommand(deleteBookingRequest))
+            ).WithTags("Session/Booking");
+    }
+}
---
+++ Module.User/Endpoints/Session/GetFutureSessions.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Module.Shared.Abstractions;
+using Module.User.Application.Features.UserSession.Query;
+
+namespace Module.User.Endpoints.Session;
+
+public class GetFutureSessions : IEndpoint
+{
+    public void MapEndpoint(WebApplication app)
+    {
+        app.MapGet("/Sessions/InFuture", async ([FromServices] IMediator mediator) =>
+            await mediator.Send(new GetFutureSessionsQuery())
+        ).WithName("Session");
+    }
+}
