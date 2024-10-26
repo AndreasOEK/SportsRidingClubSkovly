9019770109e1edf5639b1e96a9d1fc2b06727e00
TurtleDK <98598321+TurtleDK@users.noreply.github.com>
2024-10-26 21:54:45 +0200

    Http request created

---
+++ Module.User.Application/Features/UserSession/Query/GetUserPreviousBookingsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Module.User.Application.Features.UserManagement.Query.Dto;
+
+namespace Module.User.Application.Features.UserSession.Query;
+
+public record GetUserPreviousBookingsQuery(Guid Id) : IRequest<IEnumerable<UserBookingFullResponse>>;
---
+++ Module.User.Infrastructure/Features/Sessions/GetUserPreviousBookingsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Module.User.Application.Features.UserManagement.Query.Dto;
+using Module.User.Application.Features.UserSession.Query;
+using Module.User.Application.Features.UserSession.Query.Dto;
+using Module.User.Domain.Entity;
+using Module.User.Infrastructure.DbContexts;
+
+namespace Module.User.Infrastructure.Features.Sessions;
+
+public class GetUserPreviousBookingsQueryHandler : IRequestHandler<GetUserPreviousBookingsQuery, IEnumerable<UserBookingFullResponse>>
+{
+    private readonly UserDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public GetUserPreviousBookingsQueryHandler(UserDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _mapper = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Booking, UserBookingFullResponse>();
+        }).CreateMapper();
+    }
+    
+    public async Task<IEnumerable<UserBookingFullResponse>> Handle(GetUserPreviousBookingsQuery request, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Bookings
+            .AsNoTracking()
+            .Where(booking => 
+                booking.User.Id == request.Id && 
+                booking.Session.StartTime < DateTime.Now + booking.Session.Duration)
+            .ProjectTo<UserBookingFullResponse>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken: cancellationToken);
+    }
+}
