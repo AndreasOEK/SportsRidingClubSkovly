6eb5fd4264126733da0c73c2a51476ebdeb2ae7a
TurtleDK <98598321+TurtleDK@users.noreply.github.com>
2024-10-28 20:59:37 +0100

    forgot

---
+++ Module.User/Endpoints/Session/GetUserPreviousBookings.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Module.Shared.Abstractions;
+using Module.User.Application.Features.UserSession.Query;
+
+namespace Module.User.Endpoints.Session;
+
+public class GetUserPreviousBookings : IEndpoint
+{
+    public void MapEndpoint(WebApplication app)
+    {
+        app.MapGet("User/{id}/PreviousBookings", async ([FromRoute] Guid id, [FromServices] IMediator mediator) =>
+        await mediator.Send(new GetUserPreviousBookingsQuery(id)));
+    }
+}
